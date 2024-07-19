04a1bb3c64f8854b1a02caceb4e7ab841a6199e9
bompallyvijay <bompallyvijayvijay@gmail.com>
2024-07-19 18:22:30 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python manage.py collectstatic --noinput && gunicorn --workers 2 config.wsgi
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn --workers 2 config.wsgi
