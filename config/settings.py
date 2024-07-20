87062ef47ec679ac1806eaef2e1a90f6b8d1628e
NineSoftechOrg <122348658+NineSoftechOrg@users.noreply.github.com>
2024-07-20 22:42:36 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-r(vctop4h_)2ci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-ALLOWED_HOSTS = ['http://mpvyzbq2h7.ap-south-1.awsapprunner.com/', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# ALLOWED_HOSTS = ['http://mpvyzbq2h7.ap-south-1.awsapprunner.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST', 'localhost'),
+    '127.0.0.1',
+    'http://2yeqtchxwt.ap-south-1.awsapprunner.com'  # replace with your App Runner domain
+]
 
 
 # Application definition
