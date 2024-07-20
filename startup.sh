fddbb2c0e022780de6565ca9ceb47ac17e2ee374
govardhan <115453040+Govardhan62@users.noreply.github.com>
2024-07-20 19:09:12 +0530

    yaml

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 config.wsgi
+#!/bin/sh
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 config.wsgi:application --bind 0.0.0.0:8000
