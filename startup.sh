85a495d37d05603a43c4c5179e6b16563acb3bd2
SaikumarPaka <164852485+SaikumarPaka@users.noreply.github.com>
2024-07-20 18:08:41 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 config.wsgi
+#!/bin/sh
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 config.wsgi --bind 0.0.0.0:8000
+
+
