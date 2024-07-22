043a41782cedfac15bf95998eb45bc490d92ae24
SaikumarPaka <164852485+SaikumarPaka@users.noreply.github.com>
2024-07-22 17:02:18 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -84,9 +84,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
