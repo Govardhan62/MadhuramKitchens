3e0901f02be902912fc884019ed9945a6a3748c6
govardhan <115453040+Govardhan62@users.noreply.github.com>
2024-07-22 10:45:33 +0530

    update starup

--- startup.sh
+++ startup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Ensure the Python environment is set up correctly
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Apply database migrations
+echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # Start the application using gunicorn
-gunicorn --workers 2 config.wsgi:application --bind 0.0.0.0:8000
+echo "Starting the application..."
+exec waitress-serve --port=8000 config.wsgi:application
