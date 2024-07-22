f761916b0aed758ad7f5220d528df7e9d0f662aa
SaikumarPaka <164852485+SaikumarPaka@users.noreply.github.com>
2024-07-22 18:00:30 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
 # Ensure the Python environment is set up correctly
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
 # Apply database migrations
-echo "Applying database migrations..."
 python manage.py migrate --noinput
-
 # Collect static files
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start the application using Gunicorn
-echo "Starting the application..."
-exec gunicorn --workers 4 config.wsgi:application --bind 0.0.0.0:443
-
-
+# Start the application using gunicorn
+gunicorn --workers 4 config.wsgi:application --bind 0.0.0.0:443
