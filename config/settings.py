0ad648cd742be7707f43ed1eef30f48ebe0a1e89
Govardhan62 <115453040+Govardhan62@users.noreply.github.com>
2024-08-05 10:49:35 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,8 +90,25 @@ DATABASES = {
     }
 }
 
-
-
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 
 # Password validation
