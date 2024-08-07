30071be0bc579086958ee344a4803fd9c32b0265
Govardhan62 <115453040+Govardhan62@users.noreply.github.com>
2024-08-07 12:57:14 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r(vctop4h_)2ciq#!38tr(bsr^qoyr%t$7*buweve1ikl=&1%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://wvpd5atp4i.ap-south-1.awsapprunner.com/']
 
 
 # Application definition
