e53638a12200bf38a61ea0038869c18c29db64a0
Govardhan62 <115453040+Govardhan62@users.noreply.github.com>
2024-09-23 16:25:21 +0530

    Update views.py

--- madhuramkitchen/views.py
+++ madhuramkitchen/views.py
@@ -355,6 +355,7 @@ def menu_items(request):
 #         'item_quantity': bag.get(dish_id, 0)  # Return updated quantity for the item
 #     })
 
+@csrf_token
 def add_to_bag(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -440,6 +441,7 @@ def add_to_bag(request):
 
 #     return render(request, 'view_bag.html', context)
 
+@csrf_token
 def view_bag(request):
     # Retrieve the bag from the session
     bag = request.session.get('bag', [])
